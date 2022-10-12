f3aa8ff0122300414072798a04b1a374ca877d31
jiyuchen@umich.edu <jiyuchen@umich.edu>
2022-10-12 19:20:29 -0400

    add likes.py

---
+++ insta485/api/likes.py
@@ -0,0 +1,14 @@
+"""REST API for likes."""
+import flask
+import insta485
+from insta485.api import posts
+
+@insta485.app.route('/api/v1/likes/?postid=<postid>', methods=["POST"])
+def show_like(postid):
+    likeid = 
+
+    context = {
+        "likeid": likeid,
+        "url": f"/api/v1/likes/{likeid}/"
+    }
+    return flask.jsonify(**context), 201
