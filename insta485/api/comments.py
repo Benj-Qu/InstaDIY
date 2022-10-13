7e89222cc6aa2082d2590d7f8e5363a5db8d5e6f
Jiyuezh <cormaczhu@outlook.com>
2022-10-12 21:23:33 -0400

    comment

--- insta485/api/comments.py
+++ insta485/api/comments.py
@@ -7,3 +7,11 @@ from utils import check_authorization
 @insta485.app.route("/api/v1/comments/", methods=["POST"])
 def add_comment():
     postid = flask.Request.args.get("postid")
+    username, has_error, error_code = check_authorization()
+    
+    if has_error:
+        return flask.jsonify({}), error_code
+    
+    connection = insta485.model.get_db()
+    
+    
