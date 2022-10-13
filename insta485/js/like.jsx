a887cc448afe5b9b0d85bcf64fafe6a2bd35e751
jiyuchen@umich.edu <jiyuchen@umich.edu>
2022-10-13 16:45:04 -0400

    temp

--- insta485/js/like.jsx
+++ insta485/js/like.jsx
@@ -1,36 +1,33 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 class Like extends React.Component {
   /* Display image and post owner of a single post
    */
   constructor(props) {
     // Initialize mutable state
     super(props);
-    this.state = {
-      hasLiked: false,
-      numLikes: 0
-    };
+    this.state = {};
   }
 
   handleClick() {
     // const hasLiked = this.state.hasLiked;
-    const { hasLiked } = this.state;
+    const { hasLiked } = this.props;
     if (hasLiked === false)
     {
-      this.state.hasLiked = true;
-      this.state.numLikes += 1;
+      this.props.hasLiked = true;
+      this.props.numLikes += 1;
     }
     else
     {
-      this.state.hasLiked = false;
-      this.state.numLikes -= 1;
+      this.props.hasLiked = false;
+      this.props.numLikes -= 1;
     }
   }
 
   render() {
     // This line automatically assigns this.state.imgUrl to the const variable imgUrl
     // and this.state.owner to the const variable owner
-    const { hasLiked, numLikes } = this.state;
+    const { hasLiked, numLikes } = this.props;
     // Render post image and post owner
     return (
       <div>
@@ -52,5 +49,11 @@ class Like extends React.Component {
     );
   }
 }
+
+Like.propTypes = {
+  url: PropTypes.string.isRequired,
+  hasLiked: PropTypes.bool.isRequired,
+  numLikes: PropTypes.number.isRequired
+};
 
 export default Like;
