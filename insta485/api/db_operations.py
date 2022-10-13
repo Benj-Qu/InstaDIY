e88e5e154d9701cd672e30a76e22597e31117dd3
jiyuchen@umich.edu <jiyuchen@umich.edu>
2022-10-12 20:45:59 -0400

    like

---
+++ insta485/api/db_operations.py
@@ -0,0 +1,28 @@
+import insta485
+
+def has_liked(username, postid):
+    """
+    Return True if username has already liked this postid
+    else return False
+    """
+    connection = insta485.model.get_db()
+    cur = connection.execute(
+        "SELECT * "
+        "FROM likes "
+        "WHERE owner = ? AND postid = ?",
+        (username, postid)
+    )
+    return len(cur.fetchall()) != 0
+
+
+def get_likeid(username, postid):
+    """Get likeid."""
+    connection = insta485.model.get_db()
+    cur = connection.execute(
+        "SELECT likeid "
+        "FROM likes "
+        "WHERE owner = ? AND postid = ?",
+        (username, postid, )
+    )
+    results = cur.fetchone()
+    return results["likeid"]
